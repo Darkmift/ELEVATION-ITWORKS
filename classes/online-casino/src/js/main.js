037bc2eeaa6adf4017e4d229841fecaa541163a6
Darkmift <darkmift@gmail.com>
2024-04-02 17:58:29 +0300

    Update online casino UI and functionality

--- classes/online-casino/src/js/main.js
+++ classes/online-casino/src/js/main.js
@@ -1,1 +1,44 @@
-console.log('hello');
+function makeBet() {
+  return fetch('https://yesno.wtf/api')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data.answer === 'yes' ? Promise.resolve() : Promise.reject();
+    });
+}
+// function makeBet() {
+//   return new Promise((resolve, reject) => {
+//     const result = Math.random() > 0.5;
+//     console.log("🚀 ~ returnnewPromise ~ result:", result)
+
+//     result ? resolve() : reject();
+//   });
+// }
+
+// get data-bet
+const betEl = document.querySelector('[data-bet]');
+// get data-wins
+const winsEl = document.querySelector('[data-wins]');
+// get data-losess
+const lossesEl = document.querySelector('[data-losses]');
+// get data-money
+const moneyEl = document.querySelector('[data-money]');
+// get data-bank
+const bankEl = document.querySelector('[data-bank]');
+
+const betAmount = 50;
+
+betEl.addEventListener('click', () => {
+  makeBet()
+    .then(() => {
+      moneyEl.textContent = parseInt(moneyEl.textContent) + betAmount;
+      bankEl.textContent = parseInt(bankEl.textContent) - betAmount;
+      winsEl.textContent = parseInt(winsEl.textContent) + 1;
+    })
+    .catch(() => {
+      moneyEl.textContent = parseInt(moneyEl.textContent) - betAmount;
+      bankEl.textContent = parseInt(bankEl.textContent) + betAmount;
+      lossesEl.textContent = parseInt(lossesEl.textContent) + 1;
+    });
+});
