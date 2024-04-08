812a7e14b29f620b78780978221a27063f89f5e7
Darkmift <darkmift@gmail.com>
2024-04-08 18:24:13 +0300

    added todos ls demo

--- classes/localstorage-cookies/todos/src/js/main.js
+++ classes/localstorage-cookies/todos/src/js/main.js
@@ -1,10 +1,16 @@
-const state = {
-  todos: [],
-};
+import { storeToLs, readFromLS } from './utils/localstorage.js';
 
 const CLASS_LISTS = {
   INPUT: { VALID: 'input-valid', INVALID: 'input-invalid' },
   ERROR: { SHOW: 'error-show', HIDE: 'error-hide' },
+};
+
+const CONFIG = {
+  TODOS_LS_KEY: 'todos',
+};
+
+const state = {
+  todos: [],
 };
 
 const formEl = document.querySelector('.todo-form');
@@ -12,6 +18,16 @@ const titleInputEl = formEl.querySelector('#title');
 const descriptionInputEl = formEl.querySelector('#description');
 const dateInputEl = formEl.querySelector('#date');
 const todoContainerEl = document.querySelector('.todos-container');
+
+const data = readFromLS(CONFIG.TODOS_LS_KEY);
+// validate each todo
+const areValid = data?.some((todo) => {
+  return todo.id && todo.title && todo.description && todo.date && isDateValid(todo.date);
+});
+if (data && areValid) {
+  state.todos = data;
+  state.todos.forEach((todo) => renderTodo(todo));
+}
 
 formEl.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -117,6 +133,7 @@ function createTodo({ title, description, date }) {
   };
 
   state.todos.push(todo);
+  storeToLs(CONFIG.TODOS_LS_KEY, state.todos);
   renderTodo(todo);
 }
 
@@ -144,6 +161,7 @@ function removeTodo(id) {
   const todoEl = document.querySelector(`[data-todo-id="${id}"]`);
   todoEl.remove();
   state.todos = state.todos.filter((todo) => todo.id !== id);
+  storeToLs(CONFIG.TODOS_LS_KEY, state.todos);
 }
 
 function makeUUID() {
