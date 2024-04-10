038c7054eefe3b903d5648f2e874c2f302d5307e
Darkmift <darkmift@gmail.com>
2024-04-10 18:36:53 +0300

    Fix axiosInstance usage in getRates and getExchangeResult functions

--- classes/exchange-app/src/http/client.js
+++ classes/exchange-app/src/http/client.js
@@ -2,7 +2,7 @@ import axiosInstance from '.';
 
 export async function getRates(baseCurrency) {
   try {
-    return await axiosInstance.get(`/latest/${baseCurrency}`).data;
+    return (await axiosInstance.get(`/latest/${baseCurrency}`)).data;
   } catch (error) {
     console.error('Error while fetching rates', error);
     return null;
@@ -20,7 +20,7 @@ export async function getSupportedCurrencies() {
 
 export async function getExchangeResult(baseCurrency, targetCurrency, amount) {
   try {
-    return await axiosInstance.get(`/pair/${baseCurrency}/${targetCurrency}/${amount}`).data;
+    return await (axiosInstance.get(`/pair/${baseCurrency}/${targetCurrency}/${amount}`)).data;
   } catch (error) {
     console.error('Error while fetching exchange result', error);
     return null;
