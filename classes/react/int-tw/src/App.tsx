6e0a91022b9e273a39f8034f50e6c2b6983413d8
Darkmift <darkmift@gmail.com>
2024-05-15 21:13:14 +0300

    chore: Update i18n configuration and add scoped translation hook

--- classes/react/int-tw/src/App.tsx
+++ classes/react/int-tw/src/App.tsx
@@ -1,17 +1,22 @@
 import './App.css';
 import { useTranslation } from 'react-i18next';
+import { useScopedTranslation } from './hooks/useScopedTranslation';
 
-// const locales = {
-//   en: {
-//     welcom: 'Hello World!',
-//   },
-//   he: {
-//     welcom: 'שלום עולם!',
-//   },
-// };
+const locales = {
+  en: {
+    welcome: 'Hello World!',
+  },
+  he: {
+    welcome: 'שלום עולם!',
+  },
+};
 
 function App() {
-  const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation('translation', {
+    keyPrefix: 'App',
+  });
+
+  const { t: scopedt } = useScopedTranslation({ defaultLanguage: i18n.language, locales });
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
@@ -20,6 +25,7 @@ function App() {
   return (
     <div className="App">
       <h1>{t('hello')}</h1>
+      <h1>{scopedt('welcome')}</h1>
       <button onClick={() => changeLanguage('en')}>English</button>
       <button onClick={() => changeLanguage('he')}>עברית</button>
     </div>
