6f3105f59fe1eb25f77fb199ee32ff02c8ffd0ce
AndreevSV <andreevsv@gmail.com>
2024-05-26 15:44:13 +0300

    Rerstore previous state of class1/server folder. Was chaged by accident

--- classes/fullstack/class1/server/main.ts
+++ classes/fullstack/class1/server/main.ts
@@ -1,27 +1,14 @@
-import 'reflect-metadata';
-import dotenv from 'dotenv';
-import express from 'express';
-import mainRouter from './src/main.routes';
-import cookieParse from "cookie-parser"
+import 'reflect-metadata'
+import 'dotenv/config'
+import express from 'express'
+import mainRouter from './src/main.routes'
 
-dotenv.config({ path: '.env.development.local' });
+const app = express()
+const port = process.env.PORT || 3000
 
-const app = express();
-const port = process.env.PORT || 3000;
+app.use(express.json())
+app.use('/api/v1', mainRouter)
 
-
-app.use(express.json());
-app.use(cookieParse());
-app.use('/api/v1', mainRouter);
-
-const start = async () => {
-  try {
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-start();
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
