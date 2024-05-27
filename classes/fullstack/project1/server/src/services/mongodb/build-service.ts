87ae75516b8efce1f75c01422ef0928fa03b7d4a
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-05-27 15:06:51 +0300

    Use updated builds response at frontend

--- classes/fullstack/project1/server/src/services/mongodb/build-service.ts
+++ classes/fullstack/project1/server/src/services/mongodb/build-service.ts
@@ -1,6 +1,5 @@
 import { BuildCountWeekly, IBuild } from '@/types'
 import { BuildModel } from './models'
-import { formatISO } from 'date-fns'
 
 export const buildService = {
   getBuildsPaginated: async ({ page, limit }: Pagination) => {
