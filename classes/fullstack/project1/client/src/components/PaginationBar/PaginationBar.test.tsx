0c5cbedc42c82280816885e516d615ca8b96fc73
oleh-chadash <4talks@gmail.com>
2024-05-28 17:40:02 +0300

    paginationBar

--- classes/fullstack/project1/client/src/components/PaginationBar/PaginationBar.test.tsx
+++ classes/fullstack/project1/client/src/components/PaginationBar/PaginationBar.test.tsx
@@ -6,7 +6,6 @@ test('renders PaginationBar with correct page numbers', () => {
   const onPageChange = jest.fn();
   render(<PaginationBar currentPage={1} totalPages={5} onPageChange={onPageChange} />);
 
-  // Проверка отображения всех номеров страниц
   for (let i = 1; i <= 5; i++) {
     expect(screen.getByText(i)).toBeInTheDocument();
   }
@@ -16,7 +15,6 @@ test('calls onPageChange with the correct page number when a page number is clic
   const onPageChange = jest.fn();
   render(<PaginationBar currentPage={1} totalPages={5} onPageChange={onPageChange} />);
 
-  // Клик по странице 3
   fireEvent.click(screen.getByText('3'));
   expect(onPageChange).toHaveBeenCalledWith(3);
 });
@@ -25,7 +23,6 @@ test('disables previous button on first page', () => {
   const onPageChange = jest.fn();
   render(<PaginationBar currentPage={1} totalPages={5} onPageChange={onPageChange} />);
 
-  // Проверка кнопки "предыдущая страница" на первой странице
   expect(screen.getByText('<').closest('button')).toBeDisabled();
 });
 
@@ -33,6 +30,5 @@ test('disables next button on last page', () => {
   const onPageChange = jest.fn();
   render(<PaginationBar currentPage={5} totalPages={5} onPageChange={onPageChange} />);
 
-  // Проверка кнопки "следующая страница" на последней странице
   expect(screen.getByText('>').closest('button')).toBeDisabled();
 });
